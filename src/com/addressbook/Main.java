a9fed984edb4fb97e4c5132f1f7ccf909a03b761
mrunalgit98 <88592639+mrunalgit98@users.noreply.github.com>
2022-08-03 16:17:30 +0530

    Update Main.java

--- src/com/addressbook/Main.java
+++ src/com/addressbook/Main.java
@@ -1,41 +1,57 @@
 package com.addressbook;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
+  private static ArrayList<AddressSystem> list = new ArrayList<AddressSystem>();
 
-    public static void main(String[] args) {
+    public void AddContactsDetails() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the First Name  ");
+        String firstName = sc.nextLine();
+        System.out.println("Enter the Last Name  ");
+        String lastName = sc.nextLine();
+        System.out.println("Enter the City ");
+        String city = sc.nextLine();
+        System.out.println("Enter the Zip Code ");
+        String zip = sc.nextLine();
+        System.out.println("Enter the Phone Number ");
+        String phoneNumber = sc.nextLine();
 
-        System.out.println("Welcome to address Book");
-        Scanner s1=new Scanner(System.in);
-        System.out.println("Enter your firstname,lastname,city,phone number,zipcode");
-        AddressSystem c1= new AddressSystem(s1.nextLine(),s1.nextLine(),s1.nextLine(),s1.nextLong(),s1.nextInt());
-        Scanner s2=new Scanner(System.in);
-        System.out.println("Please enter the details of second contact(firstname,lastname,city,phone number,zipcode)");
-        AddressSystem c2= new AddressSystem(s2.nextLine(),s2.nextLine(),s2.nextLine(),s2.nextLong(),s2.nextInt());
-        Scanner s3=new Scanner(System.in);
-        System.out.println("Please enter the details of third contact(firstname,lastname,city,phone number,zipcode)");
-        AddressSystem c3= new AddressSystem(s3.nextLine(),s3.nextLine(),s3.nextLine(),s3.nextLong(),s2.nextInt());
+        AddressSystem details = new AddressSystem(firstName, lastName,  city, zip, phoneNumber);
+        list.add(details);
+        details.display();
+    }
+    
 
-        Scanner sc=new Scanner(System.in);
-        System.out.println("enter first name to check duplicate");
-        String name = sc.nextLine();
-        if(name.equalsIgnoreCase(c1.firstname)){
-            System.out.println("duplicate found");
-        }if(name.equalsIgnoreCase(c2.firstname)){
-            System.out.println("duplicate found");
-        }if(name.equalsIgnoreCase(c2.firstname)){
-            System.out.println("duplicate found");
-        }else {
-            System.out.println("duplicate not found");
+        private void checkDuplicateEntry () {
+            Scanner userInput = new Scanner(System.in);
+            System.out.println("Enter firstname to Check duplicate  ");
+            String name = userInput.nextLine();
+            for (AddressSystem search : list) {
+                if (name.equalsIgnoreCase(search.getFirstName())) {
+                    System.out.println("duplicate name found");
+                } else {
+                    System.out.println("duplicate name not found");
+                }
+            }
         }
 
 
-    }
+    public static void main(String[] args) {
+
+        Main book1=new Main();
+        Main book2=new Main();
+
+        System.out.println("Welcome to address Book");
+
+        book1.AddContactsDetails();
+        book2.AddContactsDetails();
+      
+        book1.checkDuplicateEntry();
 
 
-
-
+        }
 
 
 }
