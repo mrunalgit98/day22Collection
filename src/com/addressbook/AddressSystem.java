ecb0a46aef785f46482c78ef49ff660c3ec5bf77
mrunalgit98 <88592639+mrunalgit98@users.noreply.github.com>
2022-08-03 16:18:09 +0530

    Update AddressSystem.java

--- src/com/addressbook/AddressSystem.java
+++ src/com/addressbook/AddressSystem.java
@@ -2,32 +2,77 @@
 
 package com.addressbook;
 
-import java.util.Scanner;
+public class AddressSystem {
+    private String firstName = " ";
+    private String lastName = " ";
 
-class AddressSystem{
-    String firstname;
-    String lastname;
-    String city;
-    long phoneNumber;
-    int  zipcode;
+    private String city = " ";
+    private String zipcode = "";
+    private String phoneNumber = " ";
 
-    public AddressSystem(String firstname,String lastname,String city,long phoneNumber,int zipcode)
-    {
-        System.out.println("The Contact Details are as follows");
-        this.firstname=firstname;
-        this.lastname=lastname;
-        this.city=city;
-        this.phoneNumber=phoneNumber;
-        this.zipcode=zipcode;
+    public AddressSystem(String firstName, String lastName,  String city,  String zipcode,
+                              String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.city = city;
+        this.zipcode = zipcode;
+        this.phoneNumber = phoneNumber;
+    }
 
-        System.out.println("Name of the contact is : "+this.firstname+" "+this.lastname);
-        System.out.println("City is "+this.city);
-        System.out.println("Phone number= "+this.phoneNumber);
-        System.out.println("zipcode= "+this.zipcode);
+    public String getFirstName() {
+        return firstName;
+    }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
     }
 
 
 
-}
 
+    public String getZip() {
+        return zipcode;
+    }
+
+    public void setZip(String zip) {
+        this.zipcode = zipcode;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNo) {
+        this.phoneNumber = phoneNo;
+    }
+
+
+
+    public void display() {
+        System.out.println(" ");
+        System.out.println("First Name = " + firstName);
+        System.out.println("Last Name = " + lastName);
+        System.out.println("City Name = " + city);
+        System.out.println("Zip Code = " + zipcode);
+        System.out.println("Phone Number = " + phoneNumber);
+    }
+
+
+}
